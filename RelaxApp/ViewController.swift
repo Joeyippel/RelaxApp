32666f49d4b0f12a82381c502ffc4d1b2f6a51a9
Joey Ippel <joeyippel@192.168.1.108>
2016-10-04 17:04:43 +0200

    Initial Commit

---
+++ RelaxApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RelaxApp
+//
+//  Created by Joey Ippel on 04/10/2016.
+//  Copyright © 2016 Joey Ippel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
