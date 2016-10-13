7317f57c16bf8f09cf3774e1c5b1b2a7bbb0f8fe
Joey Ippel <joeyippel@192.168.1.108>
2016-10-13 09:31:17 +0200

    Version after following tutorial - 13/10/2016

---
+++ RelaxApp/Utilities.swift
@@ -0,0 +1,28 @@
+//
+//  Utilities.swift
+//  RelaxApp
+//
+//  Created by Joey Ippel on 12/10/2016.
+//  Copyright © 2016 Joey Ippel. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+// MARK: Helper Extensions
+extension UIViewController {
+  func showAlert(withTitle title: String?, message: String?) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
+}
+
+extension MKMapView {
+  func zoomToUserLocation() {
+    guard let coordinate = userLocation.location?.coordinate else { return }
+    let region = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
+    setRegion(region, animated: true)
+  }
+}
